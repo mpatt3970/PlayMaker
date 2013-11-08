0a9a9ca983db966489da55eb31b8769f9ca5adf2
Mike Patterson <mipatter@mymail.mines.edu>
2013-11-07 17:31:41 -0700

    Finished PlayMaker and Ball barebones class.

--- src/playMaker/Ball.java
+++ src/playMaker/Ball.java
@@ -1,5 +1,21 @@
 package playMaker;
 
-public class Ball {
+import java.awt.Point;
 
+public class Ball {
+	private Point currentLocation;
+	private Point targetLocation;
+	
+	public Ball(Point locationNow, Point locationEnd) {
+		this.currentLocation = locationNow;
+		this.targetLocation = locationEnd;
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void move() {
+		
+	}
 }
--- src/playMaker/PlayMaker.java
+++ src/playMaker/PlayMaker.java
@@ -19,7 +19,7 @@ public class PlayMaker extends JFrame {
 		 * 2) Repainting
 		 */
 		
-		
+
 	}
 	
 	
