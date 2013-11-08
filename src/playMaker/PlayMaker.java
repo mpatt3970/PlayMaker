9f0101ad05bcd6aa2cd8af5c2184b5a2d7c90490
Mike Patterson <mipatter@mymail.mines.edu>
2013-11-07 17:23:27 -0700

    Created the classes we sketched up in class. Going to add barebones to all classes now.

---
+++ src/playMaker/PlayMaker.java
@@ -0,0 +1,30 @@
+package playMaker;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class PlayMaker extends JFrame {
+	
+	private JPanel sideBar;
+	private JPanel field;
+	
+	PlayMaker() {
+		
+	}
+	
+	public void processPlay() {
+		/**
+		 * This function will step through the play
+		 * 1) Moving all players
+		 * 2) Repainting
+		 */
+		
+		
+	}
+	
+	
+	private static void main(String[] args0) {
+		PlayMaker gui = new PlayMaker();
+		gui.setVisible(true);
+	}
+}
---
+++ src/playMaker/Team.java
@@ -0,0 +1,5 @@
+package playMaker;
+
+public class Team {
+
+}
