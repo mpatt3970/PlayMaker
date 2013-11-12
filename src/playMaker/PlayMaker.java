9ea59289c44dcbb4d520fe4fe2ee9b75c7a3dadf
Walter Schlosser <wschloss@mymail.mines.edu>
2013-11-12 13:00:04 -0700

    Added more logic outlines to the findBestDirection function which now takes a boolean if the player is offensive or not

--- src/playMaker/PlayMaker.java
+++ src/playMaker/PlayMaker.java
@@ -14,6 +14,8 @@ public class PlayMaker extends JFrame {
 	private Team offense;
 	private Team defense;
 	
+	private int playerSize;
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
 	}
@@ -26,8 +28,9 @@ public class PlayMaker extends JFrame {
 	public void processPlay() {
 		/**
 		 * This function will step through the play
-		 * 1) Moving all players
-		 * 2) Repainting
+		 * 1) Find best direction for players
+		 * 2) Move them accordingly
+		 * 3) Repaint
 		 */
 		
 		// this handles players making appropriate movement direction choices
@@ -50,20 +53,47 @@ public class PlayMaker extends JFrame {
 	public Point findBestDirection(Player player, boolean isOffense) {
 		// this function loops over all other players to determine which direction the passed player should move
 		// it then returns that direction so it can be passed to the move function of player p
+		
+		Point distance = null;
+		
 		if (isOffense) {
 			// offensive players care about avoiding defensive players
 			for (Player p : defense.getPlayers()) {
-				Point distance = null;
 				// find distance between, find magnitude of that distance, determine if player should continue on their
 				// local direction, or return the better direction to be used in the move function
+				distance = new Point(p.getLocation().x - player.getLocation().x,p.getLocation().y - player.getLocation().y);
+				double magnitude = Math.sqrt((distance.x)^2+(distance.y)^2);
+				
+				// if statements to determine what is the best direction to return according to the magnitude of the distance
+				if (magnitude > 3*playerSize) {
+					// continue as normal, return players route direction
+				}
+				else if (magnitude > 2*playerSize) {
+					// return adjusted direction away from defensive player
+				}
+				else {
+					// 	COLLISION
+				}
 			}
 		}
 		else {
 			// defensive players care about tackling offensive
 			for (Player p : offense.getPlayers()) {
-				Point distance = null;
 				// find distance between, find magnitude of that distance, determine if player should continue on their
 				// local direction, or return the better direction to be used in the move function
+				distance = new Point(p.getLocation().x - player.getLocation().x,p.getLocation().y - player.getLocation().y);
+				double magnitude = Math.sqrt((distance.x)^2+(distance.y)^2);
+				
+				// if statements to determine what is the best direction to return according to the magnitude of the distance
+				if (magnitude > 3*playerSize) {
+					// continue as normal, return players route direction
+				}
+				else if (magnitude > 2*playerSize) {
+					// return adjusted direction away from defensive player
+				}
+				else {
+					// 	COLLISION
+				}
 			}
 		}
 		
